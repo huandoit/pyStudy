8bda37d13d2f74d5a702dfa7d1293a5b6a805504
wangoo <hi_chengzi@126.com>
2020-03-13 17:42:09 +0800

    add 3-12daily

---
+++ daily/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2020/03/13 16:50
+@Author  : WangHuan
+@Contact : hi_chengzi@126.com
+@File    : __init__.py.py
+@Software: PyCharm
+"""
---
+++ python3-cookbook/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2020/03/13 16:49
+@Author  : WangHuan
+@Contact : hi_chengzi@126.com
+@File    : __init__.py.py
+@Software: PyCharm
+"""
