9a9319c75f3f7e8a56b8b8b1e10b1d5cc0cb7687
huandoit <hi_chengzi@126.com>
2020-06-12 18:03:25 +0800

    add mock demo

---
+++ interface/flask.md
@@ -0,0 +1,3 @@
+# Flask
+> 官方文档：https://dormousehole.readthedocs.io/en/latest/
+
---
+++ interface/src/mock_api/demo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2020/6/12 17:44
+@Author  : WangHuan
+@Contact : hi_chengzi@126.com
+@File    : demo.py
+@Software: PyCharm
+@description: 被测方法
+"""
+
+
