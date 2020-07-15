5453ec9c02229ccf47ee7492035c1d157766f00e
huandoit <hi_chengzi@126.com>
2020-07-15 14:25:30 +0800

    1. 两数之和

--- leetcode/array/1. 两数之和 - 简.py
+++ leetcode/array/1. 两数之和 - 简.py
@@ -24,18 +24,23 @@
 两次for循环遍历，分别取出两个值与目标值进行比对，成功则返回，失败则进行下一轮遍历
 时间复杂度：O(n^2)
 空间复杂度：O(1)
+
+知识点：
+- range(start, stop, step)：创建一个整数列表
+    - start: 计数从 start 开始。默认是从 0 开始。例如range（5）等价于range（0， 5）
+    - stop: 计数到 stop 结束，但不包括 stop。例如：range（0， 5） 是[0, 1, 2, 3, 4]没有5
+    - step：步长，默认为1。例如：range（0， 5） 等价于 range(0, 5, 1)
 '''
-class Solution:
-    def twoSum(self, nums, target):
-        for i in range(0, len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j] == target:
-                    l = [i, j]
-                    return l
+def twoSum(nums, target):
+    for i in range(0, len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i]+nums[j] == target:
+                l = [i, j]
+                return l
 
 
 if __name__ == '__main__':
     nums = [2, 7, 11, 15]
     target = 26
-    l = Solution().twoSum(nums, target)
+    l = twoSum(nums, target)
     print(l)
