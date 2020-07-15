c7b5db9220caff08f81c3d4ce56e54043b4e3ed2
WangHuan <hi_chengzi@126.com>
2020-07-15 22:17:52 +0800

    add leetcode

---
+++ leetcode/array/27. 移除元素 - 简.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+https://leetcode-cn.com/problems/remove-element/
+给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
+示例 2:
+给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+注意这五个元素可为任意顺序。
+你不需要考虑数组中超出新长度后面的元素。
+"""
+
+'''
+思路一：
+定义指针从0开始依次遍历列表，若数值与目标值相等时删除对应值，若不相等则指针数加1继续对比下一个值
+'''
+def removeElement(self, nums, val):
+    i = 0
+    while i < len(nums):
+        if nums[i] == val:
+            nums.pop(i)
+        else:
+            i += 1
+    return len(nums)
